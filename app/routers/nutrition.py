95e705fdbf2acc36aea2f51c894dfd69b3181acd
rohit.s.shelke <rohit.s.shelke@accenture.com>
2025-01-18 20:26:47 -0500

    updated nutrition api

--- app/routers/nutrition.py
+++ app/routers/nutrition.py
@@ -4,11 +4,14 @@ import json
 
 router = APIRouter(prefix="/nutrition", tags=["nutrition"])
 
-@router.get("/nutrition")
-async def get_nutrition_info(ingredient: str):
+@router.post("/nutrition")
+async def get_nutrition_info(ingredients: dict):
     try:
-        nutri_info = await db['foods'].find_one({"description": ingredient}, {"description": 1, "foodNutrients": 1, "_id": 0})
-        return {"message": "Success", "data": json.dumps(nutri_info)}
+        nutri_info = db['foods'].find({"description": {"$in": ingredients["name"]}}, {"description": 1, "foodNutrients": 1, "_id": 0})
+        nutritional_info = []
+        async for nutri in nutri_info:
+            nutritional_info.append(nutri)
+        return {"message": "Success", "data": json.dumps(nutritional_info)}
     except Exception as e:
         print(e)
         raise HTTPException(status_code=404, detail="Nutritional info not found")
